b188856656cf256df3bdbd6e0d08187e9b42c8bc
Reidond <reidond@gmail.com>
2020-12-26 22:02:34 +0200

    feat: send small json payload

--- app/functions/gbellmf.py
+++ app/functions/gbellmf.py
@@ -2,11 +2,8 @@
 # Bell-shaped membership function
 
 from sanic import Blueprint, response
-import json
-import io
 from bokeh.plotting import figure
-from bokeh.resources import CDN
-from bokeh.embed import file_html
+from bokeh.embed import json_item
 import numpy as np
 
 gbellmf = Blueprint('functions_gbellmf', url_prefix='/gbellmf')
@@ -24,20 +21,19 @@ def gbellmf_fn(x, params):
     "POST",
 ])
 async def gbellmf_route(request):
-    data = json.load(io.BytesIO(request.body))
+    start, stop = request.json['x'].split(':')
+    x = np.linspace(int(start), int(stop))
+    a = int(request.json['a'])
+    b = int(request.json['b'])
+    c = int(request.json['c'])
 
-    start, stop = data['x'].split(':')
-    x = np.linspace(start, stop)
-
-    left_y = np.sort(
-        [gbellmf_fn(xj, [data['a'], data['b'], data['c']]) for xj in x],
-        axis=-1,
-        kind="stable")
+    left_y = np.sort([gbellmf_fn(xj, [a, b, c]) for xj in x],
+                     axis=-1,
+                     kind="stable")
     right_y = np.flip(np.sort(left_y, axis=-1, kind="stable"))
     y = [*left_y, *right_y]
 
     p = figure(plot_width=400, plot_height=400)
-    p.line(args=[np.linspace(start, stop, num=100), y],
-           kwargs={'line_width': 2})
-    html = file_html(p, CDN, "gbellmf")
-    return response.html(html)
+    p.line(np.linspace(int(start), int(stop), num=100), y, line_width=2)
+
+    return response.json(json_item(p, "gbellmf"))
