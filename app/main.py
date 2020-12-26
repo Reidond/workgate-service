282a72883a7b3589c8e06c96dfa0c7afea2fb0ac
Reidond <reidond@gmail.com>
2020-12-26 22:02:22 +0200

    fix: cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from sanic import Sanic
 from .functions import functions
+from sanic_cors import CORS
 
 app = Sanic(__name__)
 app.blueprint(functions)
+CORS(app, resources=r'/functions/*')
 
-app.run(host='0.0.0.0', port=3001, debug=True)
+app.run(host='0.0.0.0', port=3001, debug=False)
