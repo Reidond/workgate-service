13f7f2ee88cca1579f94a125364bbe42f04d5725
Reidond <reidond@gmail.com>
2020-12-27 17:35:18 +0200

    fix: heroku port

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from sanic import Sanic
 from .functions import functions
 from sanic_cors import CORS
+import os
 
 app = Sanic(__name__)
 app.blueprint(functions)
 CORS(app, resources={r'/functions/*': {"origins": "*"}})
 
-app.run(host='0.0.0.0', port=3001, debug=False)
+port = int(os.environ.get("PORT", 3001))
+app.run(host='0.0.0.0', port=port, debug=False)
